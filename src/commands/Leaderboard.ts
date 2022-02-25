822728c3d683c1a0407337a7918fafab46b0c9f4
raziman <raziman.terra@gmail.com>
2022-02-26 03:03:09 +0800

    leaderboard of XP instead of coins

--- src/commands/Leaderboard.ts
+++ src/commands/Leaderboard.ts
@@ -2,25 +2,25 @@ import { Command } from "@jiman24/commandment";
 import { Message } from "discord.js";
 import { client } from "../index";
 import { MessageEmbed } from "../structure/MessageEmbed";
-import { bold, currency } from "../utils";
+import { bold } from "../utils";
 
 export default class extends Command {
   name = "leaderboard";
   aliases = ["l"];
-  description = "show leaderboard of rich players";
+  description = "show leaderboard of players xp";
 
   exec(msg: Message) {
 
     const player = client.players.array()
-      .sort((a, b) => b.coins - a.coins)
-      .map((x, i) => `${i + 1}. ${x.name} \`${x.coins}\``)
+      .sort((a, b) => b.xp - a.xp)
+      .map((x, i) => `${i + 1}. ${x.name} \`${x.xp}\``)
       .slice(0, 10)
       .join("\n");
 
     const embed = new MessageEmbed(msg.author)
       .setColor("RANDOM")
       .setTitle("Leaderboard")
-      .setDescription(bold(`Name | ${currency}\n`) + player);
+      .setDescription(bold(`Name | XP\n`) + player);
 
     msg.channel.send({ embeds: [embed] });
   }
